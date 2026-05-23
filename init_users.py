20efd54d57f7f252f5e84a10c64cbd54902fd168
Felix_Du <112299616+Sonder-on@users.noreply.github.com>
2026-05-23 14:29:10 +0800

    Update init_users.py

--- init_users.py
+++ init_users.py
@@ -13,12 +13,6 @@ with app.app_context():
         db.session.add(admin_user)
         print("✅ 管理员账号 (admin) 准备创建...")
 
-    # 检查 dxl 是否存在
-    if not User.query.filter_by(username='dxl').first():
-        dxl_user = User(username='dxl', password=encrypt_md5('1234'), role='user')
-        db.session.add(dxl_user)
-        print("✅ 普通账号 (dxl) 准备创建...")
-
     # 提交到数据库
     db.session.commit()
     print("🎉 初始用户检查/创建成功！")
